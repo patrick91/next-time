ecb725b877dc1e02ee62fc73c1ec868efc5c26d3
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-08 12:56:51 +0000

    Update title

--- app/revalidate/page.tsx
+++ app/revalidate/page.tsx
@@ -6,7 +6,7 @@ export default async function Revlaidate() {
     <div className="grid items-center justify-center min-h-screen bg-gray-50">
       <div className="space-y-12 p-4">
         <h1 className="text-4xl font-bold text-center underline">
-          Force static
+          Default with revalidation on components
         </h1>
         <NowRevalidate />
 
