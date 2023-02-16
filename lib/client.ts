8937b93a3be8b51446348e794cda11d314240232
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-16 16:28:01 +0000

    Recreate client on the server

--- lib/client.ts
+++ lib/client.ts
@@ -3,11 +3,12 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 let client: ApolloClient<any> | null = null;
 
 export const getClient = () => {
-  if (!client) {
+  // create a new client if there's no existing one
+  // or if we are running on the server.
+  if (!client || typeof window === "undefined") {
     let defaultOptions = {};
 
     if (typeof window === "undefined") {
-      // don't use
       defaultOptions = {
         watchQuery: {
           fetchPolicy: "no-cache",
@@ -21,7 +22,6 @@ export const getClient = () => {
     }
 
     client = new ApolloClient({
-      ssrMode: typeof window === "undefined",
       link: new HttpLink({
         uri: "https://main--time-pav6zq.apollographos.net/graphql",
       }),
